7d0d389cb7d326cf3ba701e65d6da344f89b481a
Yoshihisa Murakami <muranet+git@gmail.com>
2017-09-10 23:10:42 +0900

    Ex4- 5 回答

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
   before_action :load_ticket, only: %i(edit update show)
+  before_action :redirect_root_if_used, only: %i(edit update)
 
   def index
     redirect_to root_path
@@ -26,6 +27,7 @@ class TicketsController < ApplicationController
   end
 
   def update
+    return if @ticket.used?
     if @ticket.update(ticket_update_params)
       redirect_to root_path, notice: '降車しました。😄'
     else
@@ -45,5 +47,9 @@ class TicketsController < ApplicationController
 
   def load_ticket
     @ticket = Ticket.find(params[:id])
+  end
+
+  def redirect_root_if_used
+    return redirect_to root_path, notice: '降車済みの切符です。' if @ticket.used?
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,15 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+  validate :is_valid_fare?, on: :update
+
+  def is_valid_fare?
+    unless self.exited_gate.exit?(self)
+      errors.add(:exited_gate, "では降車できません。")
+    end
+  end
+
+  def used?
+    !exited_gate.nil?
+  end
 end
